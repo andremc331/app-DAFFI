6635828576abcfcac8c20d4b16a7a33492a331da
andremc331 <andremc331@gmail.com>
2024-12-13 12:22:13 -0300

    pesquisa funcionando

---
+++ back/dist/config/database.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var sequelize_1 = require("sequelize");
+// Criação da instância do Sequelize para se conectar ao PostgreSQL
+var sequelize = new sequelize_1.Sequelize({
+    dialect: 'postgres',
+    host: 'localhost', // Host onde o banco de dados está rodando
+    port: 5432, // Porta padrão do PostgreSQL
+    username: 'postgres', // Nome de usuário do banco de dados
+    password: '123', // Senha do banco de dados
+    database: 'daffi', // Nome do banco de dados
+    logging: false, // Desabilitar logs (opcional)
+});
+// Testar a conexão
+sequelize.authenticate()
+    .then(function () {
+    console.log('Conexão com o banco de dados realizada com sucesso!');
+})
+    .catch(function (err) {
+    console.error('Erro ao conectar com o banco de dados:', err);
+});
+sequelize.sync({ force: false }) // O parâmetro "force: false" garante que ele não apagará as tabelas existentes
+    .then(function () {
+    console.log('Modelos sincronizados com o banco de dados.');
+})
+    .catch(function (err) {
+    console.error('Erro ao sincronizar modelos:', err);
+});
+exports.default = sequelize;
